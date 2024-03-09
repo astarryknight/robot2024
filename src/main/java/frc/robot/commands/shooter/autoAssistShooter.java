77cc14428622e0305a4e7ca871bbeff537cd9307
Starry Knight <82652543+astarryknight@users.noreply.github.com>
2024-03-09 01:32:06 +0000

    begin shooting while driving command?

---
+++ src/main/java/frc/robot/commands/shooter/autoAssistShooter.java
@@ -0,0 +1,10 @@
+package frc.robot.commands.shooter;
+
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.swerve.SwerveModule;
+
+
+public class autoAssistShooter(){
+	
+} 
